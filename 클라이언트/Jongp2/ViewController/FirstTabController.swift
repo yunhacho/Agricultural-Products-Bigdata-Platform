b98e43bce6868fac13f77ded18e09768e732d355
JunseokNoh <40800157+JunseokNoh@users.noreply.github.com>
2021-05-30 23:20:59 +0900

    add git ignore

---
+++ 클라이언트/Jongp2/ViewController/FirstTabController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstTabController.swift
+//  Jongp2
+//
+//  Created by junseok on 2021/05/30.
+//
+
+
+import UIKit
+
+class FirstTabController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
